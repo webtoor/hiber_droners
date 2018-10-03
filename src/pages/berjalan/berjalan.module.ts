4dfcffa6610dcd06f744758ca6161808966760ec
fityanalimunshi@gmail.com <fityanalimunshi@gmail.com>
2018-10-04 03:00:12 +0700

    page berjalan

---
+++ src/pages/berjalan/berjalan.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { BerjalanPage } from './berjalan';
+
+@NgModule({
+  declarations: [
+    BerjalanPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(BerjalanPage),
+  ],
+})
+export class BerjalanPageModule {}
---
+++ src/pages/berjalan/berjalan.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+/**
+ * Generated class for the BerjalanPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-berjalan',
+  templateUrl: 'berjalan.html',
+})
+export class BerjalanPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad BerjalanPage');
+  }
+
+}
