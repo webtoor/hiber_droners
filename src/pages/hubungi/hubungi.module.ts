2aad21a780f612eeb513e6fbc68f1e2d4745bea7
fityanalimunshi@gmail.com <fityanalimunshi@gmail.com>
2018-10-04 16:19:01 +0700

    page hubungi

---
+++ src/pages/hubungi/hubungi.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { HubungiPage } from './hubungi';
+
+@NgModule({
+  declarations: [
+    HubungiPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(HubungiPage),
+  ],
+})
+export class HubungiPageModule {}
