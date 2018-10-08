17f3294182645f685afc4697b62a9b14f4935a8f
fityanalimunshi@gmail.com <fityanalimunshi@gmail.com>
2018-10-08 13:59:43 +0700

    loader, toast, login

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { MenuController, NavController, NavParams, LoadingController, ToastController, Events  } from 'ionic-angular';
 import { TabsPage } from '../tabs/tabs';
+import { RestApiProvider } from '../../providers/rest-api/rest-api'
+
 /**
  * Generated class for the LoginPage page.
  *
@@ -14,8 +16,10 @@ import { TabsPage } from '../tabs/tabs';
   templateUrl: 'login.html',
 })
 export class LoginPage {
+  responseData: any;
+  loading: any;
   userData = { "email": "", "password": "" };
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public authService: RestApiProvider, public menu: MenuController, private toastCtrl: ToastController, public loadingCtrl: LoadingController, public events: Events) {
   }
 
   ionViewDidLoad() {
@@ -23,7 +27,54 @@ export class LoginPage {
   }
 
   login(){
-    this.navCtrl.setRoot(TabsPage)
+    if (this.userData.email && this.userData.password) {
+      this.authService.postData(this.userData, "login", "").then((result) => {
+        this.responseData = result;
+        console.log(this.responseData);
+        if (this.responseData["access_token"]) {
+          this.showLoader();
+          this.events.publish('email', this.responseData.email);
+
+          localStorage.setItem('userData', JSON.stringify(this.responseData));
+          this.loading.dismiss();
+          this.navCtrl.setRoot(TabsPage);
+        }
+        else{
+         this.presentToast('Invalid credentials' + "\n" +  this.responseData['message']);
+        }
+      }, (err) => {
+        this.showLoader();
+        this.loading.dismiss();
+        this.presentToast("Tidak terhubung ke server");
+      });
+    }
+    else {
+      this.presentToast("The email field is required \n The password field is required");
+    }
+  }
+
+  showLoader() {
+    this.loading = this.loadingCtrl.create({
+      content: 'Authentication...',
+      duration: 3000,
+    });
+
+    this.loading.present();
+  }
+
+  presentToast(msg) {
+    let toast = this.toastCtrl.create({
+      message: msg,
+      duration: 3000,
+      position: 'bottom',
+      dismissOnPageChange: true
+    });
+
+    toast.onDidDismiss(() => {
+      console.log('Dismissed toast');
+    });
+
+    toast.present();
   }
 
 }
