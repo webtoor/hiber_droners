94ed43e07d8a801dbcf031db70c7f38109db88f6
fityanalimunshi@gmail.com <fityanalimunshi@gmail.com>
2018-10-10 16:42:10 +0700

    name : Hiber Droners

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,9 +3,6 @@ import { Nav, Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
-import { HomePage } from '../pages/home/home';
-import { ListPage } from '../pages/list/list';
-import { TabsPage } from '../pages/tabs/tabs';
 import { AkunPage } from '../pages/akun/akun';
 import { LoginPage } from '../pages/login/login';
 import { HubungiPage } from '../pages/hubungi/hubungi';
