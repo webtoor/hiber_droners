eee339fa922b77f7d37d46c960635b6ebf73042a
fityanalimunshi@gmail.com <fityanalimunshi@gmail.com>
2018-10-29 13:24:30 +0700

    install @types/google-maps

--- src/pages/detail-tawaran/detail-tawaran.ts
+++ src/pages/detail-tawaran/detail-tawaran.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { NavController, NavParams, LoadingController, App } from 'ionic-angular';
 import { RestApiProvider } from '../../providers/rest-api/rest-api';
 import { LoginPage } from '../login/login';
@@ -22,6 +22,8 @@ export class DetailTawaranPage {
   loading:any
   subject:any;
   order_id:any;
+  map:any;
+  @ViewChild('map') mapElement: ElementRef;
   constructor(public authService: RestApiProvider, public loadingCtrl: LoadingController, public app: App,public navCtrl: NavController, public navParams: NavParams) {
     this.subject= navParams.get('subject');
     this.order_id= navParams.get('order_id');
@@ -39,6 +41,34 @@ export class DetailTawaranPage {
       console.log(this.responseData);
       if(this.responseData['success'] == true){
         this.items = this.responseData['data'];
+          
+    let LatLng = new google.maps.LatLng(this.responseData['polygon'][0]['latitude'], this.responseData['polygon'][0]['longitude']);
+
+    let mapOptions = {
+      center:LatLng,
+      zoom:17,
+      MapTypeID: google.maps.MapTypeId.ROADMAP
+    };
+
+    this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+
+      var arr = this.responseData['polygon'];
+      var cords = []
+      for (var i = 0; i < arr.length; i++) {
+        cords.push(new google.maps.LatLng(parseFloat(this.responseData['polygon'][i]['latitude']), parseFloat(this.responseData['polygon'][i]['longitude'])));
+      }
+      //console.log(cords)
+        new google.maps.Polygon({
+           paths: cords,
+           map: this.map,
+           strokeColor: '#000',
+           strokeOpacity: 0.8,
+           strokeWeight: 6,
+           fillColor: 'green',
+           fillOpacity: 0.35,
+         });
+         this.map.getBounds(cords);
+         cords = [];
         this.loading.dismiss()
       }else{
         this.loading.dismiss()
