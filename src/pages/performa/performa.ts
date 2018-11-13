97b9f92784a4d09ea655b50b70d45a83f1511543
webtoor <fityanalimunshi@gmail.com>
2018-11-13 09:37:20 +0700

    handler + showLoader

--- src/pages/performa/performa.ts
+++ src/pages/performa/performa.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams, LoadingController  } from 'ionic-angular';
+import { NavController, NavParams, LoadingController, App  } from 'ionic-angular';
 import { RestApiProvider } from '../../providers/rest-api/rest-api';
+import { LoginPage } from '../login/login';
 
 /**
  * Generated class for the PerformaPage page.
@@ -15,12 +16,32 @@ import { RestApiProvider } from '../../providers/rest-api/rest-api';
   templateUrl: 'performa.html',
 })
 export class PerformaPage {
-
-  constructor(public authService: RestApiProvider, public loadingCtrl: LoadingController, public navCtrl: NavController, public navParams: NavParams) {
+  public userDetails : any;
+  public responseData: any;
+  public items : any;
+  loading:any;
+  constructor(public app: App, public authService: RestApiProvider, public loadingCtrl: LoadingController, public navCtrl: NavController, public navParams: NavParams) {
+    const data = JSON.parse(localStorage.getItem('userProvider'));
+    this.userDetails = data;
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad PerformaPage');
   }
+
+  backToWelcome(){
+    let nav = this.app.getRootNav();
+    nav.setRoot(LoginPage);
+   }
+
+   showLoader() {
+    this.loading = this.loadingCtrl.create({
+      spinner: 'ios',
+      content: 'Loading..',
+    });
+
+    this.loading.present();
+  }
+  
 
 }
